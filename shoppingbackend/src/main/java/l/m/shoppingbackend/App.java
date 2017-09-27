504baa704ac9a4e9717d17e7a95279558b55d73e
Lucian Murgu <murgu_lucian@yahoo.com>
2017-09-27 03:39:29 +0300

    project setup

---
+++ shoppingbackend/src/main/java/l/m/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package l.m.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
